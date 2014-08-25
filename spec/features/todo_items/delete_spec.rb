74b02344b1c85925dd2702046165b1495cc9ffa3
RJ McCollam <rj@wedodev.com>
2014-08-25 15:32:41 -0500

    Delete todo list items
    Signed-off-by: RJ McCollam <rj@wedodev.com>

---
+++ spec/features/todo_items/delete_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Editing todo items" do
+
+	let!(:todo_list) { TodoList.create(title: "Grocery List", description: "Groceries") }
+	let!(:todo_list) { todo_list.todo_items.create(content: "Milk") }
+
+	def visit_todo_list(list)
+		visit "/todo_lists"
+		within "#todo_list_#{todo_list.id}" do
+			click_link "List Items"
+		end
+	end
+
+	it "is successful" do
+		visit_todo_list(todo_list)
+		within "#todo_item#{todo_item.id}" do
+			click_link "Delete"
+		end
+		expect(page).to have_content("Todo list item was deleted.")
+		expect(TodoItems.count).to eq(0)
+	end
+
+end
